f1d099230aaee7db4120de9ea3115a2e62205b67
kui <thebazz.98@gmail.com>
2019-12-20 20:28:54 +0200

    added toggalble (is that a word?) navbar and to be continued sections

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
 /* eslint linebreak-style: ["error", "windows"] */
+
+// eslint-disable-next-line no-unused-vars
+const toggleTabs = (() => {
+  const burger = document.querySelector('.navbar-burger');
+  const sidebar = document.querySelector('.navbar-menu');
+  const tabs = document.querySelectorAll('.navbar-item');
+  const tabsContent = document.querySelectorAll('.section');
+
+  const deactivateAllTabs = () => {
+    tabs.forEach((tab) => {
+      tab.classList.remove('is-active');
+    });
+  };
+
+  const hideTabsContent = () => {
+    tabsContent.forEach((tabContent) => {
+      tabContent.classList.remove('is-active');
+    });
+  };
+
+  const getIndex = (el) => [...el.parentElement.children].indexOf(el);
+
+  const activateTabsContent = (tab) => {
+    tabsContent[getIndex(tab)].classList.add('is-active');
+  };
+
+  tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+      deactivateAllTabs();
+      hideTabsContent();
+      activateTabsContent(tab);
+      tab.classList.add('is-active');
+    });
+  });
+
+  burger.addEventListener('click', () => {
+    sidebar.classList.toggle('is-active');
+  });
+
+  tabs[0].click();
+})();
